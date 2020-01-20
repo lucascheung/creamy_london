818a7af6facc5231618d269a742ad3c422f77ca8
Lucas Cheung <lucascheungls@gmail.com>
2020-01-20 22:48:34 +0000

    first push

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'middleman', '~> 3.4'
+
+# Deploy to GitHub Pages
+gem 'middleman-deploy', '~> 1.0'
+
+# Assets
+gem 'jquery-middleman'
+gem 'bootstrap-sass', '3.3.6'
+gem 'font-awesome-sass', '~> 5.8.1'
+gem 'middleman-autoprefixer'
+
+gem 'middleman-dotenv'
+gem 'middleman-s3_sync'
+gem 'mime-types'
---
+++ config.rb
@@ -0,0 +1,113 @@
+###
+# Compass
+###
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+# Auto-prefixing of CSS code with vendor prefix
+activate :autoprefixer
+
+activate :dotenv
+activate :directory_indexes
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+# configure :development do
+#   activate :livereload
+# end
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  activate :relative_assets
+  set :relative_links, true
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+  #
+  require_relative "./lib/build_cleaner"
+  activate :build_cleaner
+end
+
+# Deployment
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.build_before = true
+
+  # Optional Settings
+  # deploy.remote = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
+
+# S3 Sync Details
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = 'www.arrowglobal.io' # The name of the S3 bucket you are targeting. This is globally unique.
+  s3_sync.region                     = 'eu-west-2'     # The AWS region for your bucket.
+  s3_sync.aws_access_key_id          = ENV['AWS_SECRET_ACCESS_KEY']
+  s3_sync.aws_secret_access_key      = ENV['AWS_ACCESS_KEY_ID']
+  s3_sync.delete                     = false # We delete stray files by default.
+  s3_sync.after_build                = false # We do not chain after the build step by default.
+  s3_sync.prefer_gzip                = true
+  s3_sync.path_style                 = true
+  s3_sync.reduced_redundancy_storage = false
+  s3_sync.acl                        = 'public-read'
+  s3_sync.encryption                 = false
+  s3_sync.prefix                     = ''
+  s3_sync.version_bucket             = false
+  s3_sync.index_document             = 'index.html'
+  s3_sync.error_document             = '404.html'
+end
