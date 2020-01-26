112a9442a398340b7db09ce14e073eed7881ce45
Lucas <lucas@mightyhive.com>
2020-01-26 02:41:14 +0000

    first version done

--- source/javascripts/navbar.js
+++ source/javascripts/navbar.js
@@ -1,46 +1,46 @@
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  // addClasses();
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.querySelector(".navbar-wagon-brand img").style.width = "30px";
-    document.querySelector(".navbar-wagon-brand-small img").style.width = "30px";
-    document.querySelector(".dropdown-toggle").style.padding = "20px";
-    document.querySelector('.navbar-wagon').style.height = "40px";
-  } else {
-    document.querySelector(".navbar-wagon-brand img").style.width = "40px";
-    document.querySelector(".navbar-wagon-brand-small img").style.width = "40px";
-    document.querySelector(".dropdown-toggle").style.padding = "35px";
-    document.querySelector('.navbar-wagon').style.height = "50px";
-  }
-};
+// function scrollFunction() {
+//   // addClasses();
+//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//     document.querySelector(".navbar-wagon-brand img").style.width = "30px";
+//     document.querySelector(".navbar-wagon-brand-small img").style.width = "30px";
+//     document.querySelector(".dropdown-toggle").style.padding = "20px";
+//     document.querySelector('.navbar-wagon').style.height = "40px";
+//   } else {
+//     document.querySelector(".navbar-wagon-brand img").style.width = "40px";
+//     document.querySelector(".navbar-wagon-brand-small img").style.width = "40px";
+//     document.querySelector(".dropdown-toggle").style.padding = "35px";
+//     document.querySelector('.navbar-wagon').style.height = "50px";
+//   }
+// };
 
 
-function addClasses() {
-  const intro = document.querySelector('.intro-container');
-  const about = document.querySelector('.about-content');
-  const service = document.querySelector('.service-content');
-  if (isScrolledIntoView(intro)) {
-    intro.classList.remove('hidden');
-    intro.classList.add('fadeIn');
-  }
-  if (isScrolledIntoView(about)) {
-    about.classList.remove('hidden');
-    about.classList.add('slideLeft');
-  }
-  if (isScrolledIntoView(service)) {
-    service.classList.remove('hidden');
-    service.classList.add('slideRight');
-  }
-};
+// function addClasses() {
+//   const intro = document.querySelector('.intro-container');
+//   const about = document.querySelector('.about-content');
+//   const service = document.querySelector('.service-content');
+//   if (isScrolledIntoView(intro)) {
+//     intro.classList.remove('hidden');
+//     intro.classList.add('fadeIn');
+//   }
+//   if (isScrolledIntoView(about)) {
+//     about.classList.remove('hidden');
+//     about.classList.add('slideLeft');
+//   }
+//   if (isScrolledIntoView(service)) {
+//     service.classList.remove('hidden');
+//     service.classList.add('slideRight');
+//   }
+// };
 
-function isScrolledIntoView(elem) {
-  var docViewTop = $(window).scrollTop();
-  var docViewBottom = docViewTop + $(window).height();
+// function isScrolledIntoView(elem) {
+//   var docViewTop = $(window).scrollTop();
+//   var docViewBottom = docViewTop + $(window).height();
 
-  var elemTop = $(elem).offset().top;
-  var elemBottom = elemTop + $(elem).height();
+//   var elemTop = $(elem).offset().top;
+//   var elemBottom = elemTop + $(elem).height();
 
-  return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-};
+//   return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+// };
 
