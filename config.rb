fdc1efdf07f5f84d47e52a3f421dd41ac52ce719
Lucas <lucas@mightyhive.com>
2020-01-27 00:15:45 +0000

    not sure

--- config.rb
+++ config.rb
@@ -94,20 +94,20 @@ activate :deploy do |deploy|
 end
 
 # S3 Sync Details
-activate :s3_sync do |s3_sync|
-  s3_sync.bucket                     = 'www.arrowglobal.io' # The name of the S3 bucket you are targeting. This is globally unique.
-  s3_sync.region                     = 'eu-west-2'     # The AWS region for your bucket.
-  s3_sync.aws_access_key_id          = ENV['AWS_SECRET_ACCESS_KEY']
-  s3_sync.aws_secret_access_key      = ENV['AWS_ACCESS_KEY_ID']
-  s3_sync.delete                     = false # We delete stray files by default.
-  s3_sync.after_build                = false # We do not chain after the build step by default.
-  s3_sync.prefer_gzip                = true
-  s3_sync.path_style                 = true
-  s3_sync.reduced_redundancy_storage = false
-  s3_sync.acl                        = 'public-read'
-  s3_sync.encryption                 = false
-  s3_sync.prefix                     = ''
-  s3_sync.version_bucket             = false
-  s3_sync.index_document             = 'index.html'
-  s3_sync.error_document             = '404.html'
-end
+# activate :s3_sync do |s3_sync|
+#   s3_sync.bucket                     = 'www.arrowglobal.io' # The name of the S3 bucket you are targeting. This is globally unique.
+#   s3_sync.region                     = 'eu-west-2'     # The AWS region for your bucket.
+#   s3_sync.aws_access_key_id          = ENV['AWS_SECRET_ACCESS_KEY']
+#   s3_sync.aws_secret_access_key      = ENV['AWS_ACCESS_KEY_ID']
+#   s3_sync.delete                     = false # We delete stray files by default.
+#   s3_sync.after_build                = false # We do not chain after the build step by default.
+#   s3_sync.prefer_gzip                = true
+#   s3_sync.path_style                 = true
+#   s3_sync.reduced_redundancy_storage = false
+#   s3_sync.acl                        = 'public-read'
+#   s3_sync.encryption                 = false
+#   s3_sync.prefix                     = ''
+#   s3_sync.version_bucket             = false
+#   s3_sync.index_document             = 'index.html'
+#   s3_sync.error_document             = '404.html'
+# end
